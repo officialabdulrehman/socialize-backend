eeb69269f291e432945138024cda6b542cfb743f
Abdul Rehman Nizamani <arehman08@hotmail.com>
2021-08-18 22:47:14 +0500

    some adjustments

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
-import axios from "axios";
 import { graphqlHTTP } from "express-graphql";
 import { graphqlSchema } from "./graphql/schema/schema";
 import { graphqlResolver } from "./graphql/resolvers/resolver";
-
+import { authMiddleware } from "./middlewares/authMiddleware";
+import { recievers } from "./recievers";
+import { MONGODB_URI, PORT } from "./utils/secrets";
+import { io } from "./socket";
+import { cors } from "./utils/cors";
 // import postRoutes from './routes/postRoutes';
 // import userRoutes from './routes/userRoutes';
-import { authMiddleware } from "./middlewares/authMiddleware";
-import recievers from "./recievers";
-import { MONGODB_URI, SERVER_URL, PORT } from "./utils/secrets";
-import { io } from "./socket";
 
 const app = express();
 
-axios.defaults.baseURL = SERVER_URL;
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
-});
+app.use(cors);
 app.use(express.json());
-
 app.use(authMiddleware);
 
 app.use(
@@ -59,45 +46,41 @@ app.use(
 //   const { statusCode, message, data } = err
 //   res.status(statusCode || 500).json({ message: message, data: data })
 // })
+try {
+  const db = await mongoose.connect(MONGODB_URI.toString());
+  const server = app.listen(PORT || 8080);
+  console.log(`Server running at PORT: ${PORT || 8080}`);
+  const myio = io.init(server);
+  myio.on("connection", (socket) => {
+    console.log("CLIENT CONNECTED", socket.id);
 
-mongoose
-  .connect(MONGODB_URI.toString())
-  .then((result) => {
-    const server = app.listen(PORT || 8080);
-    console.log(`Server running at PORT: ${PORT}`);
-    const myio = io.init(server);
-    myio.on("connection", (socket) => {
-      console.log("CLIENT CONNECTED", socket.id);
+    socket.emit("message", `Welcome ${socket.id}`);
 
-      socket.emit("message", `Welcome ${socket.id}`);
+    socket.broadcast.emit(
+      "message",
+      `Fellas, say hi to our nakama ${socket.id}`
+    );
 
+    console.log("userId = " + socket.handshake.query.userId);
+    let notificationReciever = socket.handshake.query.userId;
+    if (notificationReciever !== "undefined") {
+      recievers.push(notificationReciever);
+    }
+
+    socket.on("disconnect", () => {
+      if (notificationReciever !== undefined) {
+        let index = recievers.findIndex(
+          (reciever) => reciever === notificationReciever
+        );
+        recievers.splice(index, 1);
+      }
+      console.log(`${socket.id} disconnected`);
       socket.broadcast.emit(
         "message",
-        `Fellas, say hi to our nakama ${socket.id}`
+        `Fellas, we have lost our nakama ${socket.id}`
       );
-
-      console.log("userId = " + socket.handshake.query.userId);
-      let notificationReciever = socket.handshake.query.userId;
-      if (notificationReciever !== "undefined") {
-        recievers.push(notificationReciever);
-      }
-
-      socket.on("disconnect", () => {
-        if (notificationReciever !== undefined) {
-          let index = recievers.findIndex(
-            (reciever) => reciever === notificationReciever
-          );
-          recievers.splice(index, 1);
-        }
-        console.log(`${socket.id} disconnected`);
-        socket.broadcast.emit(
-          "message",
-          `Fellas, we have lost our nakama ${socket.id}`
-        );
-      });
     });
-  })
-  .catch((err) => {
-    console.log(err);
-    //throw new Error("Database connection error");
   });
+} catch (err) {
+  throw new Error("Database connection error");
+}
---
+++ utils/cors.js
@@ -0,0 +1,11 @@
+export const cors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+};
+export default cors;
